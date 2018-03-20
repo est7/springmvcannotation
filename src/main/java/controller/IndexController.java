d386223d18b1a211c5a05bada3f769877147eca4
lihang <lihang@droi.com>
2018-03-20 20:52:12 +0800

    访问路径/check，可以不停刷新session中记录的访问次数,Session在用户登录，一些特殊场合在页面间传递数据的时候会经常用到

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by Administrator.
@@ -20,18 +21,33 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 class IndexController {
     @RequestMapping("/index")
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response){
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView index = new ModelAndView("index");
         index.addObject("message", "Hello S11Pring MVC");
         return index;
     }
 
 
-
     @RequestMapping("/jump")
-    public ModelAndView jump(){
+    public ModelAndView jump() {
         //redirect:/index 表示客户端跳转到index意思
         ModelAndView mav = new ModelAndView("redirect:/addProduct11");
+        return mav;
+    }
+
+    @RequestMapping("/check")
+    public ModelAndView check(HttpSession session) {
+        //映射 /check 到方法check()
+        //为方法check()提供参数HttpSession session，这样就可以在方法体中使用session了
+       // 接下来的逻辑就是每次访问为session中的count+1.
+      //  最后跳转到check.jsp页面
+        Integer i = (Integer) session.getAttribute("count");
+        if (i == null) {
+            i = 0;
+        }
+        i++;
+        session.setAttribute("count", i);
+        ModelAndView mav = new ModelAndView("check");
         return mav;
     }
 
