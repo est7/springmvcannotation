f4d93192152bf7f51c9688bb2393b72ba163d224
lihang <lihang@droi.com>
2018-03-20 20:19:05 +0800

    使用spring提交表单

---
+++ src/main/java/controller/IndexController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Administrator.
+ * <p>
+ * Created Time : 2018/3/20 0:58.
+ * <p>
+ * Description : File in controller , Project in SpringMvcM
+ * <p>
+ * Content:
+ */
+
+@Controller
+class IndexController {
+    @RequestMapping("/index")
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response){
+        ModelAndView index = new ModelAndView("index");
+        index.addObject("message", "Hello S11Pring MVC");
+        return index;
+    }
+}
+
---
+++ src/main/java/pojo/Product.java
@@ -0,0 +1,39 @@
+package pojo;
+
+/**
+ * Created by Administrator.
+ * <p>
+ * Created Time : 2018/3/20 0:57.
+ * <p>
+ * Description : File in pojo , Project in SpringMvcM
+ * <p>
+ * Content:
+ * @author Administrator
+ */
+
+public class Product {
+    private int id;
+    private String name;
+    private float price;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public float getPrice() {
+        return price;
+    }
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+
+}
+
