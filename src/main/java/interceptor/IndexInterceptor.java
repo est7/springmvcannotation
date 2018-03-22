d24bef01538ce59ca071e80ec9fefac12e2973ef
lihang <lihang@droi.com>
2018-03-22 20:23:25 +0800

    SPRING MVC拦截器,要注意preHandle方法return 值的不同所代表的情况

---
+++ src/main/java/interceptor/IndexInterceptor.java
@@ -0,0 +1,51 @@
+package interceptor;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Administrator.
+ * <p>
+ * Created Time : 2018/3/22 20:10.
+ * <p>
+ * Description : File in interceptor , Project in springmvcannotation
+ * <p>
+ * Content:
+ */
+
+class IndexInterceptor extends HandlerInterceptorAdapter {
+    /**
+     * 在业务处理器处理请求之前被调用
+     * 如果返回false
+     *     从当前的拦截器往回执行所有拦截器的afterCompletion(),再退出拦截器链
+     * 如果返回true
+     *    执行下一个拦截器,直到所有的拦截器都执行完毕
+     *    再执行被拦截的Controller
+     *    然后进入拦截器链,
+     *    从最后一个拦截器往回执行所有的postHandle()
+     *    接着再从最后一个拦截器往回执行所有的afterCompletion()
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("preHandle(), 在访问Controller之前被调用");
+        return true;
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle(), 在访问Controller之后，访问视图之前被调用,这里可以注入一个时间到modelAndView中，用于后续视图显示");
+        modelAndView.addObject("date","由拦截器生成的时间:" + new Date());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        System.out.println("afterCompletion(), 在访问视图之后被调用");
+    }
+}
